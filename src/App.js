a6d2bd4f21fc2bb6e627ca344725b10e2be45c45
Nesrine <kabachinesrine34@gmail.com>
2024-09-08 16:16:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import Shop from './Pages/Shop';
-import Popular from './Components/Popular/Popular';
 import Browse from './Components/Browse/Browse';
-import Product from './Pages/Product';
+ 
  
 
 function App() {
@@ -15,11 +13,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/Shop' element={<Shop />}/>
-        <Route path='/Popular' element={<Popular />}/>
-        <Route path='/Product' element={<Product />}/>
-        <Route path='/:productId' element={<Product />}/>
- 
         <Route path='/Browse' element={<Browse />}/>
  
 
